7cf607d6b46fa2625c686436869794060059d12e
Nijjwal Shrestha <Nijjwal Shrestha@DESKTOP-BN6MRAN.hsd1.nm.comcast.net>
2024-12-16 23:33:24 -0700

    udemy-1: project test

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+	@GetMapping("/data")
+	public String getBadData() {
+		return "don't code like this!";
+	}
+
+}
